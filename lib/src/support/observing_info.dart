c63cb3e060bea4c2b9f2bdb41054f82392fbff98
Mikhail Zotyev <mbixjkee1392@gmail.com>
2025-08-08 10:19:22 +0200

    Feat: improve presenting observing info.

--- lib/src/support/observing_info.dart
+++ lib/src/support/observing_info.dart
@@ -51,27 +51,78 @@ class _Subscibers extends StatelessWidget {
             }
 
             return Column(
-              children: data.entries.map(
-                (entry) {
-                  final type = entry.key;
-                  final subscribers = entry.value;
-                  return ExpansionTile(
-                    title: Text(type.toString()),
-                    children: subscribers.entries.map((subEntry) {
-                      final key = subEntry.key;
-                      final subscriberSet = subEntry.value;
-                      return ListTile(
-                        title:
-                            Text('$key: ${subscriberSet.length} subscribers'),
-                      );
-                    }).toList(),
-                  );
-                },
-              ).toList(),
+              children: data.entries
+                  .map(
+                    (entry) => _ScopeGroupSubscribers(
+                      name: entry.key.toString(),
+                      taggedSubscribers: entry.value,
+                    ),
+                  )
+                  .toList(),
             );
           },
         ),
       ],
+    );
+  }
+}
+
+class _ScopeGroupSubscribers extends StatelessWidget {
+  final String name;
+  final Map<Object?, Set<Object>> taggedSubscribers;
+
+  const _ScopeGroupSubscribers({
+    required this.name,
+    required this.taggedSubscribers,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      title: Text(name),
+      children: taggedSubscribers.entries.map((subEntry) {
+        final key = subEntry.key;
+        final tag = key == null ? 'untagged' : key.toString();
+        final subscriberSet = subEntry.value;
+
+        return ListTile(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('$tag: ${subscriberSet.length} subscribers'),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: DecoratedBox(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      left: BorderSide(
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Wrap(
+                      children: subscriberSet
+                          .map(
+                            (e) => Text(
+                              e.toString(),
+                              style: const TextStyle(
+                                fontWeight: FontWeight.normal,
+                                fontSize: 12,
+                                fontStyle: FontStyle.italic,
+                              ),
+                            ),
+                          )
+                          .toList(),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      }).toList(),
     );
   }
 }
