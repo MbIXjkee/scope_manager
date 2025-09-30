20f40f691199b0387d7e313740e325f40c992447
Mikhail Zotyev <mbixjkee1392@gmail.com>
2025-09-30 12:33:24 +0200

    Docs: add documentation for the registry part.

--- lib/src/base/scope_registry.dart
+++ lib/src/base/scope_registry.dart
@@ -1,25 +1,37 @@
 import 'package:scope_manager/src/base/dependency_scope.dart';
 import 'package:scope_manager/src/base/scope_resolver.dart';
 
+/// A type definition for a factory function that creates
+/// instances of [FeatureScope].
 typedef ScopeFactory<S extends FeatureScope> = S Function(
   ScopeResolver resolver,
 );
 
 // observable may cause additional performance pressure.
+/// An interface for registering available dependency scopes.
 abstract interface class ScopeRegistry {
+  /// Initializes the registry.
+  /// 
+  /// Sets up the [RootScope] and optionally registers additional
+  /// [FeatureScope] bindings to create them on demand.
   Future<void> init(
     RootScope rootScope, {
     List<ScopeBinding>? bindings,
+    // TODO(mjk): move this to observer.
     bool observable,
   });
 
+  /// Registers a binding for a [FeatureScope] type to its factory.
   void registerScopeBinding<S extends FeatureScope>(
     ScopeBinding<S> binding,
   );
 
+  /// Registers a factory function for creating instances of a specific
+  /// [FeatureScope] type.
   void registerScopeFactory<S extends FeatureScope>(ScopeFactory<S> factory);
 }
 
+/// A supporting class that binds a [FeatureScope] type to its factory.
 class ScopeBinding<S extends FeatureScope> {
   final ScopeFactory<S> factory;
 
